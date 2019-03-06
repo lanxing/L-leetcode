290a150348876c20f7e552cc29bd45e9cb2704e4
lanxing <1013131239@qq.com>
2019-03-06 20:27:41 +0800

    update

--- 33.search-in-rotated-sorted-array.java
+++ 33.search-in-rotated-sorted-array.java
@@ -39,7 +39,42 @@
  */
 class Solution {
     public int search(int[] nums, int target) {
-        return searchB(nums, target, 0, nums.length - 1);
+        return solution2(nums, target);
+    }
+
+    public int solution2(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        int mid;
+        while (left <= right){
+            mid = (left + right) / 2;
+            if (nums[mid] == target){
+                return mid;
+            }
+//            在递增数组内
+            if (nums[left] < nums[right]){
+                if (nums[mid] > target){
+                    right = mid - 1;
+                }else {
+                    left = mid + 1;
+                }
+            }else {
+                //左边是递增的
+                if (nums[mid] > nums[right]){
+                    if (nums[left] <= target && target < nums[mid]){
+                        right = mid - 1;
+                    }else {
+                        left = mid + 1;
+                    }
+                }else {
+                    if (nums[mid] < target && target <= nums[right]){
+                        left = mid + 1;
+                    }else {
+                        right = mid - 1;
+                    }
+                }
+            }
+        }
+        return -1;
     }
 
     public int searchB(int[] nums, int target, int left, int right){
---
+++ 82.remove-duplicates-from-sorted-list-ii.java
@@ -0,0 +1,44 @@
+/*
+ * @lc app=leetcode id=82 lang=java
+ *
+ * [82] Remove Duplicates from Sorted List II
+ *
+ * https://leetcode.com/problems/remove-duplicates-from-sorted-list-ii/description/
+ *
+ * algorithms
+ * Medium (32.10%)
+ * Total Accepted:    169.3K
+ * Total Submissions: 525.5K
+ * Testcase Example:  '[1,2,3,3,4,4,5]'
+ *
+ * Given a sorted linked list, delete all nodes that have duplicate numbers,
+ * leaving only distinct numbers from the original list.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: 1->2->3->3->4->4->5
+ * Output: 1->2->5
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: 1->1->1->2->3
+ * Output: 2->3
+ * 
+ * 
+ */
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        
+    }
+}

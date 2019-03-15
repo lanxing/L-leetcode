afe6dfff9155508fa3ba19771d7e6386ff1d8eb0
lanxing <1013131239@qq.com>
2019-03-15 16:14:57 +0800

    update

--- 100.same-tree.java
+++ 100.same-tree.java
@@ -48,6 +48,8 @@
  * 
  * 
  */
+
+
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -59,6 +61,19 @@
  */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        
+        return isSame(p, q);
+    }
+
+    public boolean isSame(TreeNode p, TreeNode q){
+        if (p == null && q == null){
+            return true;
+        }
+        if (p == null || q == null){
+            return false;
+        }
+        if (p.val != q.val){
+            return false;
+        }
+        return isSame(p.left, q.left) && isSame(p.right, q.right);
     }
 }

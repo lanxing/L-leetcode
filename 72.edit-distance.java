ed9bb4537552cf5e459a05a2ab67f61770781463
lanxing <1013131239@qq.com>
2019-03-20 19:56:27 +0800

    update

--- 72.edit-distance.java
+++ 72.edit-distance.java
@@ -1,54 +1,72 @@
 /*
- * @lc app=leetcode id=72 lang=java
+ * @lc app=leetcode.cn id=72 lang=java
  *
- * [72] Edit Distance
+ * [72] 编辑距离
  *
- * https://leetcode.com/problems/edit-distance/description/
+ * https://leetcode-cn.com/problems/edit-distance/description/
  *
  * algorithms
- * Hard (36.30%)
- * Total Accepted:    158.4K
- * Total Submissions: 434.8K
+ * Hard (48.13%)
+ * Total Accepted:    4.4K
+ * Total Submissions: 9.1K
  * Testcase Example:  '"horse"\n"ros"'
  *
- * Given two words word1 and word2, find the minimum number of operations
- * required to convert word1 to word2.
+ * 给定两个单词 word1 和 word2，计算出将 word1 转换成 word2 所使用的最少操作数 。
  * 
- * You have the following 3 operations permitted on a word:
- * 
- * 
- * Insert a character
- * Delete a character
- * Replace a character
+ * 你可以对一个单词进行如下三种操作：
  * 
  * 
- * Example 1:
+ * 插入一个字符
+ * 删除一个字符
+ * 替换一个字符
  * 
  * 
- * Input: word1 = "horse", word2 = "ros"
- * Output: 3
- * Explanation: 
- * horse -> rorse (replace 'h' with 'r')
- * rorse -> rose (remove 'r')
- * rose -> ros (remove 'e')
+ * 示例 1:
+ * 
+ * 输入: word1 = "horse", word2 = "ros"
+ * 输出: 3
+ * 解释: 
+ * horse -> rorse (将 'h' 替换为 'r')
+ * rorse -> rose (删除 'r')
+ * rose -> ros (删除 'e')
  * 
  * 
- * Example 2:
+ * 示例 2:
  * 
- * 
- * Input: word1 = "intention", word2 = "execution"
- * Output: 5
- * Explanation: 
- * intention -> inention (remove 't')
- * inention -> enention (replace 'i' with 'e')
- * enention -> exention (replace 'n' with 'x')
- * exention -> exection (replace 'n' with 'c')
- * exection -> execution (insert 'u')
+ * 输入: word1 = "intention", word2 = "execution"
+ * 输出: 5
+ * 解释: 
+ * intention -> inention (删除 't')
+ * inention -> enention (将 'i' 替换为 'e')
+ * enention -> exention (将 'n' 替换为 'x')
+ * exention -> exection (将 'n' 替换为 'c')
+ * exection -> execution (插入 'u')
  * 
  * 
  */
 class Solution {
     public int minDistance(String word1, String word2) {
-        
+        int len1 = word1.length();
+        int len2 = word2.length();
+        int[][] dp = new int[len1 + 1][len2 + 1];
+        dp[0][0] = 0;
+        int k = 1;
+        for (int i = 1; i <= len1; ++i){
+            dp[i][0] = k++;
+        }
+        k = 1;
+        for (int i = 1; i <= len2; ++i){
+            dp[0][i] = k++;
+        }
+        for (int i = 1; i <= len1; ++i){
+            for (int j  = 1; j <= len2; ++j){
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)){
+                    dp[i][j] = dp[i - 1][j - 1];
+                }else {
+                    dp[i][j] = Math.min(dp[i - 1][j], Math.min(dp[i - 1][j - 1], dp[i][j - 1])) + 1;
+                }
+            }
+        }
+        return dp[len1][len2];
     }
 }

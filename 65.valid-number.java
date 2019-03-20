0283c2193938d16c7f8c88f4d327f8145039cd62
lanxing <1013131239@qq.com>
2019-03-20 19:21:34 +0800

    update

--- 65.valid-number.java
+++ 65.valid-number.java
@@ -1,51 +1,29 @@
 /*
- * @lc app=leetcode id=65 lang=java
+ * @lc app=leetcode.cn id=65 lang=java
  *
- * [65] Valid Number
+ * [65] 有效数字
  *
- * https://leetcode.com/problems/valid-number/description/
+ * https://leetcode-cn.com/problems/valid-number/description/
  *
  * algorithms
- * Hard (13.75%)
- * Total Accepted:    114.6K
- * Total Submissions: 831.3K
+ * Hard (13.93%)
+ * Total Accepted:    2.5K
+ * Total Submissions: 17.8K
  * Testcase Example:  '"0"'
  *
- * Validate if a given string can be interpreted as a decimal number.
+ * 验证给定的字符串是否为数字。
  * 
- * Some examples:
+ * 例如:
  * "0" => true
  * " 0.1 " => true
  * "abc" => false
  * "1 a" => false
  * "2e10" => true
- * " -90e3   " => true
- * " 1e" => false
- * "e3" => false
- * " 6e-1" => true
- * " 99e2.5 " => false
- * "53.5e93" => true
- * " --6 " => false
- * "-+3" => false
- * "95a54e53" => false
  * 
- * Note: It is intended for the problem statement to be ambiguous. You should
- * gather all requirements up front before implementing one. However, here is a
- * list of characters that can be in a valid decimal number:
+ * 说明: 我们有意将问题陈述地比较模糊。在实现代码之前，你应当事先思考所有可能的情况。
  * 
- * 
- * Numbers 0-9
- * Exponent - "e"
- * Positive/negative sign - "+"/"-"
- * Decimal point - "."
- * 
- * 
- * Of course, the context of these characters also matters in the input.
- * 
- * Update (2015-02-10):
- * The signature of the C++ function had been updated. If you still see your
- * function signature accepts a const char * argument, please click the reload
- * button to reset your code definition.
+ * 更新于 2015-02-10:
+ * C++函数的形式已经更新了。如果你仍然看见你的函数接收 const char * 类型的参数，请点击重载按钮重置你的代码。
  * 
  */
 class Solution {
--- 68.text-justification.java
+++ 68.text-justification.java
@@ -1,48 +1,41 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /*
- * @lc app=leetcode id=68 lang=java
+ * @lc app=leetcode.cn id=68 lang=java
  *
- * [68] Text Justification
+ * [68] 文本左右对齐
  *
- * https://leetcode.com/problems/text-justification/description/
+ * https://leetcode-cn.com/problems/text-justification/description/
  *
  * algorithms
- * Hard (22.37%)
- * Total Accepted:    89.9K
- * Total Submissions: 399.9K
+ * Hard (35.50%)
+ * Total Accepted:    1.5K
+ * Total Submissions: 4.1K
  * Testcase Example:  '["This", "is", "an", "example", "of", "text", "justification."]\n16'
  *
- * Given an array of words and a width maxWidth, format the text such that each
- * line has exactly maxWidth characters and is fully (left and right)
- * justified.
+ * 给定一个单词数组和一个长度 maxWidth，重新排版单词，使其成为每行恰好有 maxWidth 个字符，且左右两端对齐的文本。
  * 
- * You should pack your words in a greedy approach; that is, pack as many words
- * as you can in each line. Pad extra spaces ' ' when necessary so that each
- * line has exactly maxWidth characters.
+ * 你应该使用“贪心算法”来放置给定的单词；也就是说，尽可能多地往每行中放置单词。必要时可用空格 ' ' 填充，使得每行恰好有 maxWidth 个字符。
  * 
- * Extra spaces between words should be distributed as evenly as possible. If
- * the number of spaces on a line do not divide evenly between words, the empty
- * slots on the left will be assigned more spaces than the slots on the right.
+ * 要求尽可能均匀分配单词间的空格数量。如果某一行单词间的空格不能均匀分配，则左侧放置的空格数要多于右侧的空格数。
  * 
- * For the last line of text, it should be left justified and no extra space is
- * inserted between words.
+ * 文本的最后一行应为左对齐，且单词之间不插入额外的空格。
  * 
- * Note:
+ * 说明:
  * 
  * 
- * A word is defined as a character sequence consisting of non-space characters
- * only.
- * Each word's length is guaranteed to be greater than 0 and not exceed
- * maxWidth.
- * The input array words contains at least one word.
+ * 单词是指由非空格字符组成的字符序列。
+ * 每个单词的长度大于 0，小于等于 maxWidth。
+ * 输入单词数组 words 至少包含一个单词。
  * 
  * 
- * Example 1:
+ * 示例:
  * 
- * 
- * Input:
+ * 输入:
  * words = ["This", "is", "an", "example", "of", "text", "justification."]
  * maxWidth = 16
- * Output:
+ * 输出:
  * [
  * "This    is    an",
  * "example  of text",
@@ -50,34 +43,30 @@
  * ]
  * 
  * 
- * Example 2:
+ * 示例 2:
  * 
- * 
- * Input:
+ * 输入:
  * words = ["What","must","be","acknowledgment","shall","be"]
  * maxWidth = 16
- * Output:
+ * 输出:
  * [
  * "What   must   be",
  * "acknowledgment  ",
  * "shall be        "
  * ]
- * Explanation: Note that the last line is "shall be    " instead of "shall
- * be",
- * because the last line must be left-justified instead of fully-justified.
- * ⁠            Note that the second line is also left-justified becase it
- * contains only one word.
+ * 解释: 注意最后一行的格式应为 "shall be    " 而不是 "shall     be",
+ * 因为最后一行应为左对齐，而不是左右两端对齐。       
+ * ⁠    第二行同样为左对齐，这是因为这行只包含一个单词。
  * 
  * 
- * Example 3:
+ * 示例 3:
  * 
- * 
- * Input:
+ * 输入:
  * words =
  * ["Science","is","what","we","understand","well","enough","to","explain",
  * "to","a","computer.","Art","is","everything","else","we","do"]
  * maxWidth = 20
- * Output:
+ * 输出:
  * [
  * "Science  is  what we",
  * ⁠ "understand      well",
@@ -91,6 +80,68 @@
  */
 class Solution {
     public List<String> fullJustify(String[] words, int maxWidth) {
-        
+        List<String> result = new ArrayList<>();
+        int i = 0;
+        while (i < words.length){
+            StringBuffer buffer = new StringBuffer();
+            int len = 0;
+            int j = i;
+            while (j < words.length){
+                if (len + words[j].length() + j - i > maxWidth){
+                    break;
+                }
+                len += words[j].length();
+                ++j;
+            }
+            int wordsCount = j - i;
+            int spaceCount = wordsCount - 1;
+            int spaceLen = maxWidth - len;
+            int eachSpaceCount = 0;
+            int outSpaceCount = 0;
+            if (spaceCount != 0){
+                eachSpaceCount = spaceLen / spaceCount;
+                outSpaceCount = spaceLen % spaceCount;
+            }
+            int spaceIndex = 0;
+            if (j >= words.length){
+                //最后一行
+                for (int k = i; k < j; ++k){
+                    buffer.append(words[k]);
+                    if (k != j - 1){
+                        buffer.append(" ");
+                    }
+                }
+                int num = maxWidth - buffer.toString().length();
+                while (num > 0){
+                    buffer.append(" ");
+                    num--;
+                }
+            }else {
+                for (int k = i; k < j; ++k){
+                    if (k == j - 1){
+                        buffer.append(words[k]);
+                    }else {
+                        buffer.append(words[k]);
+                        spaceIndex = 0;
+                        while (spaceIndex < eachSpaceCount){
+                            spaceIndex++;
+                            buffer.append(" ");
+                        }
+                        if (outSpaceCount > 0){
+                            buffer.append(" ");
+                            outSpaceCount--;
+                        }
+                    }
+                }
+                int num = maxWidth - buffer.toString().length();
+                while (num > 0){
+                    buffer.append(" ");
+                    num--;
+                }
+            }
+            i = j;
+            result.add(buffer.toString());
+        }
+        return result;
     }
 }

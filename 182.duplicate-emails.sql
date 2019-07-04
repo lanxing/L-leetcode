8de71b3b9ab47a74704d5a16ba5cbc0f9d163b4b
lanxing <1013131239@qq.com>
2019-07-04 20:21:00 +0800

    update

---
+++ 182.duplicate-emails.sql
@@ -0,0 +1,42 @@
+--
+-- @lc app=leetcode.cn id=182 lang=mysql
+--
+-- [182] 查找重复的电子邮箱
+--
+-- https://leetcode-cn.com/problems/duplicate-emails/description/
+--
+-- database
+-- Easy (71.95%)
+-- Total Accepted:    30.9K
+-- Total Submissions: 41K
+-- Testcase Example:  '{"headers": {"Person": ["Id", "Email"]}, "rows": {"Person": [[1, "a@b.com"], [2, "c@d.com"], [3, "a@b.com"]]}}'
+--
+-- 编写一个 SQL 查询，查找 Person 表中所有重复的电子邮箱。
+-- 
+-- 示例：
+-- 
+-- +----+---------+
+-- | Id | Email   |
+-- +----+---------+
+-- | 1  | a@b.com |
+-- | 2  | c@d.com |
+-- | 3  | a@b.com |
+-- +----+---------+
+-- 
+-- 
+-- 根据以上输入，你的查询应返回以下结果：
+-- 
+-- +---------+
+-- | Email   |
+-- +---------+
+-- | a@b.com |
+-- +---------+
+-- 
+-- 
+-- 说明：所有电子邮箱都是小写字母。
+-- 
+--
+# Write your MySQL query statement below
+
+select distinct Email from (select distinct Email, count(Email) as c from Person group by Email) m where m.c > 1
+
